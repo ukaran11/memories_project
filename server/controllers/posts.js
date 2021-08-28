e775483909f7d3d23835bc3ec8ce832d1543ffb2
ukaran11 <ukaran11@gmail.com>
2021-08-28 17:24:26 +0530

    Utsav | Added posts routes and posts controller | Added PostMessage Model

---
+++ server/controllers/posts.js
@@ -0,0 +1,7 @@
+export const getPosts = (req, res) => {
+    res.send('THIS WORKS!');
+}
+
+export const createPost = (req, res) => {
+    res.send('Post Creation')
+}
--- server/index.js
+++ server/index.js
@@ -3,8 +3,10 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+import postRoutes from './routes/posts.js';
 const app = express();
 
+app.use('/posts', postRoutes);
 app.use(express.json({limit: "30mb", extended: true}));
 app.use(express.urlencoded({limit: "30mb", extended: true}));
 app.use(cors());
