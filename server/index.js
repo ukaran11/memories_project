778f7e32fcf2b668efda8c65a8dbcd3b8c7aebd8
ukaran11 <ukaran11@gmail.com>
2021-09-02 00:15:52 +0530

    Utsav | Deployed to Heroku

--- server/index.js
+++ server/index.js
@@ -11,7 +11,12 @@ dotenv.config();
 app.use(express.json({limit: "30mb", extended: true}));
 app.use(express.urlencoded({limit: "30mb", extended: true}));
 app.use(cors());
+
 app.use('/posts', postRoutes);
+
+app.get('/', (req, res) => {
+    res.send('Hello to Memories API');
+})
 
 const CONNECTION_URL = process.env.CONNECTION_URL;
 const PORT = process.env.PORT || 5000;
