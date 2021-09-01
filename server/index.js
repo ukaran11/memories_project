52ec833ff959923f31c3318fe8112861527d3b89
ukaran11 <ukaran11@gmail.com>
2021-09-01 23:43:58 +0530

    Utsav | Used dotenv .env file to hide db and port config

--- server/index.js
+++ server/index.js
@@ -2,17 +2,18 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
 import postRoutes from './routes/posts.js';
+
 const app = express();
-
-
+dotenv.config();
 app.use(express.json({limit: "30mb", extended: true}));
 app.use(express.urlencoded({limit: "30mb", extended: true}));
 app.use(cors());
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://test12345:test12345@cluster0.bdwrb.mongodb.net/memories_project?retryWrites=true&w=majority'
+const CONNECTION_URL = process.env.CONNECTION_URL;
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { 
