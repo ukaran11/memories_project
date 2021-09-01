9ebe9943fe483508ea34a5a2344c3bd939a0d074
ukaran11 <ukaran11@gmail.com>
2021-09-01 19:41:38 +0530

    Utsav | Added likes adding functionality to backend

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -13,3 +13,7 @@ export const createPost = (newPost) => {
 export const updatePost = (id, updatedPost) => {
     return axios.patch(`${url}/${id}`, updatedPost);
 }
+
+export const deletePost = (id) => {
+    return axios.delete(`${url}/${id}`);
+}
--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -43,4 +43,16 @@ export const deletePost = async (req, res) => {
     await PostMessage.findByIdAndRemove(_id);
 
     res.json({ message: 'Post deleted successfully'});
+    console.log("post deleted")
+}
+
+export const likePost = async (req, res) => {
+    const { id: _id } = req.params;
+
+    if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that id');
+
+    const post = await PostMessage.findById(id);
+    const updatedPost = await PostMessage.findByIdAndUpdate(id, {likeCount: post.likeCount +1}, { new: true})
+
+    res.json(updatedPost);
 }
